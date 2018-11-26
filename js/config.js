e561a9cae84486fcb4fbfb5162221a8f5bbf9e5a
许骜 <xuao@hikvision.com>
2018-11-26 15:44:28 +0800

    [MOD] v2.6.3-deta

--- js/config.js
+++ js/config.js
@@ -62,7 +62,7 @@ var config = {
     repo: 'agreatao.github.io',
     title: '骜 - 做一个了不起的人',
     titleLoad: '骜 - 正在加载...',
-    token: '792bb4f01e5c41f1d0c020908bf478943765118b'
+    token: '11753d053d72991d5058fa69abebd58ae0fc9444'
 };
 
 module.exports = (0, _extends3.default)({}, config, {
